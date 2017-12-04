740c012b907a535c5d3634d63f54f804c9b8d666
Colin Campbell <colin.campbell@digitalistgroup.com>
2017-12-04 07:18:30 +0100

    Fix install autoloader

--- web/install.php
+++ web/install.php
@@ -19,7 +19,7 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require __DIR__.'/../app/autoload.php';
 //Debug::enable();
 
 // Enable APC for autoloading to improve performance.
