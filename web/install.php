259bf989feabd284931aa464c8609d01fd95eac9
Colin Campbell <colin.campbell@digitalistgroup.com>
2017-12-10 17:26:58 +0100

    Remove deprecated loadClassCache() from installer

--- web/install.php
+++ web/install.php
@@ -33,11 +33,10 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/InstallKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+
 
 $kernel = new InstallKernel('install', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
